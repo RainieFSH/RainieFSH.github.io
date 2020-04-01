75c11c92f5cc8dea5641e5f8edef75f8e6c9e192
Sihui Feng <60416330+RainieFSH@users.noreply.github.com>
2020-04-01 15:39:50 -0400

    h2

--- asst_2/main.js
+++ asst_2/main.js
@@ -5,9 +5,29 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+/*Copy from https://mdn.github.io/learning-area/javascript/building-blocks/gallery/ */
+for(let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+      displayedImage.src = e.target.src;
+    }
+  }
 
 /* Wiring up the Darken/Lighten button */
+/*Copy from https://mdn.github.io/learning-area/javascript/building-blocks/gallery/ */
+
+btn.onclick = function() {
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  }
+  
