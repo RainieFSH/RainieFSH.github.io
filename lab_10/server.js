e8f3cde0f962e5e13c4075ead7e01ab0fc7c50ed
Sihui Feng <60416330+RainieFSH@users.noreply.github.com>
2020-04-25 18:03:33 -0400

    10

--- lab_10/server.js
+++ lab_10/server.js
@@ -3,8 +3,10 @@
 import express from "express";
 import fetch from "node-fetch";
 
-// const sqlite3 = require('sqlite3').verbose(); // We're including a server-side version of SQLite, the in-memory SQL server.
-// const open = require(sqlite).open; // We're including a server-side version of SQLite, the in-memory SQL server.
+const sqlite3 = require('sqlite3').verbose(); // We're including a server-side version of SQLite, the in-memory SQL server.
+const express = require('express');
+const fetch = require('node-fetch');
+const open = require(sqlite).open; // We're including a server-side version of SQLite, the in-memory SQL server.
 
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
@@ -18,12 +20,20 @@ const dbSettings = {
 const app = express();
 const port = process.env.PORT || 3000;
 
+const db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error('err.message');
+  }
+  console.log('Connected to the memory SQL database');
+});
+db.run('CREATE TABLE responses(name text, zipcode text, interests text)');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ""; // Enter the URL for the data you would like to retrieve here
+  const baseURL = db; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
@@ -38,6 +48,35 @@ function processDataForFrontEnd(req, res) {
       console.log(err);
       res.redirect("/error");
     });
+}
+
+function insertData(req, res) {
+  const input = req.body;
+  const { name, zip_code, interests } = input;
+  db.run('INSERT INTO responses (name, zipcode, interests) VALUES ("' + name + '","' + zip_code + '","' + interests +'")',
+    (err) => {
+      if (err) {
+        console.log(err.message);
+      }
+      res.json({ message: 'Your submission has been recorded', form: req.body});
+    });
+}
+function returnData(res) {
+  const base = 'SELECT * FROM responses';
+  db.all(base, [], (err, rows) => {
+    if (err) { throw err; }
+    const data = ({ 'records': [] });
+    for (let i = 0; i < rows.length; i++) {
+      const row = rows[i];
+      data.records.push({
+        'name': row.name,
+        'zip': row.zipcode,
+        'interest': row.interests
+      });
+    }
+    res.json(data);
+  }
+  );
 }
 
 // Syntax change - we don't want to repeat ourselves,
